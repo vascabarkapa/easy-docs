7f7172b59ea3327a4d8002fbd081b64c57a020cd
vascabarkapa <vascabarkapa@gmail.com>
2024-06-22 18:17:26 +0200

    footer fixed

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function updateDateTime() {
+    var now = new Date();
+    var dateTimeString = now.toLocaleString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: false
+    });
+    document.getElementById('currentDateTime').textContent = dateTimeString;
+}
+
+setInterval(updateDateTime, 1000);
